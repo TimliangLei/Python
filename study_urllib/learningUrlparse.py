fb758b42bb01326444dddc1112e9b14e17177b79
TimliangLei <timlianglei@126.com>
2019-04-26 10:46:00 +0800

    更新Urlparse

--- study_urllib/learningUrlparse.py
+++ study_urllib/learningUrlparse.py
@@ -2,3 +2,9 @@ from urllib.parse import urlparse
 
 result = urlparse('http://www.baidu.com/index.html;user?id=5#comment')
 print(type(result),result)
+
+result = urlparse('www.baidu.com/index.html;user?id=5#commnt',scheme='https')
+print(result)
+
+result = urlparse('http://www.baidu.com/index.html;user?id=5#commnt',scheme='https')
+print(result)
